fccec2c9ed3d127923b99280359475a0469828f2
Salenia <vsreui123456@gmail.com>
2022-12-08 11:51:56 +0200

    Init commit

---
+++ apps/site/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class='text-slate-300'>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
